4e3a491bc235b7e12997c75ed4e239ad39779f25
EthanNoy <ethan@ethannoy.com>
2017-10-11 13:57:59 -0700

    We made a buble sort class, and documented it.

---
+++ Ethan/BubbleSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ethan
+{
+    public static class BubbleSort
+    {
+
+        /// <summary>
+        /// Sort the specified list using bubblesort.
+        /// </summary>
+        /// <returns>The sort.</returns>
+        /// <param name="values">A list of integers to sort.</param>
+        public static void Sort (List<int> values) {
+            
+            for (int left_address = 0; left_address < values.Count; left_address++)
+            {
+
+                for (int right_address = values.Count - 1; right_address > left_address; right_address--)
+                {
+                    if (values[left_address] > values[right_address])
+                    {
+                        int left_value = values[left_address];
+                        int right_value = values[right_address];
+                        values[left_address] = right_value;
+                        values[right_address] = left_value;
+
+
+                    }
+                }
+            }
+
+        }
+
+
+    }
+}
