5e2cba4c9c51080949805ad848ea9e8ffe92cb9b
EthanNoy <ethan@ethannoy.com>
2018-05-09 14:29:07 -0700

    Implemented remove, added HW due 5/14/18

---
+++ Ethan/DoubleLinkedList.cs
@@ -0,0 +1,189 @@
+﻿using System;
+namespace Ethan
+{
+	public class DoubleLinkedList
+	{
+
+		public Node Head
+		{
+			get;
+			private set;
+		}
+
+		public Node Tail
+		{
+			get;
+			private set;
+		}
+
+		public int Count
+		{
+			get
+			{
+				int count = 0;
+				Node traveler = Head;
+				if (Head == null)
+				{
+					return 0;
+				}
+				while (traveler != null)
+				{
+					count = count + 1;
+					traveler = traveler.Next;
+				}
+				return count;
+			}
+		}
+
+		public DoubleLinkedList()
+		{
+		}
+
+		public void AddToHead(int value)
+		{
+			Node NewNode = new Node();
+			NewNode.Value = value;
+
+			if (Head == null && Tail == null)
+			{
+				Head = NewNode;
+				Tail = NewNode;
+				return;
+
+			}
+
+			else
+			{
+				NewNode.Next = Head;
+				Head.Previous = NewNode;
+				Head = NewNode;
+
+			}
+
+		}
+
+		public void AddToTail(int value)
+		{
+			Node NewNode = new Node();
+			NewNode.Value = value;
+
+			if (Head == null && Tail == null)
+			{
+				Head = NewNode;
+				Tail = NewNode;
+				return;
+
+			}
+
+			else
+			{
+				NewNode.Previous = Tail;
+				Tail.Next = NewNode;
+				Tail = NewNode;
+			}
+		}
+
+		public void Remove(int value)
+		{
+
+			Node traveler = Head;
+            Node before_traveler = null;
+            Node after_traveler = null;
+            while (traveler != null)
+            {
+				before_traveler = traveler.Previous;
+				after_traveler = traveler.Next;
+
+                if (traveler.Value != value)
+                {
+                    traveler = traveler.Next;
+                }
+                else if (traveler.Value == value)
+                {
+                    if (traveler == Tail){
+                        before_traveler.Next = null;
+                        traveler.Previous = null;
+                        Tail = before_traveler;
+						traveler = null;
+                    }
+                    
+                    else if (traveler == Head){
+                        after_traveler.Previous = null;
+                        traveler.Next = null;
+                        Head = after_traveler;
+						traveler = null;
+                    }
+
+					else if (traveler == Head && traveler == Tail)
+					{
+						Head = null;
+						Tail = null;
+						traveler = null;
+					}
+					else
+					{
+						before_traveler.Next = after_traveler;
+						after_traveler.Previous = before_traveler;
+                        traveler.Next = null;
+						traveler.Previous = null;
+						return;
+					}
+				}
+
+			}
+		}
+
+		public void RemoveAll(int value) 
+		{
+			throw new NotImplementedException();	
+		}
+
+		public void RemoveFirst()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RemoveLast()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string ToString()
+		{
+			System.Text.StringBuilder builder = new System.Text.StringBuilder();
+
+			builder.Append("From Head : ");
+
+			Node traveler = Head;
+			while (traveler != null)
+			{
+				builder.Append(traveler.Value);
+				builder.Append(" -> ");
+				traveler = traveler.Next;
+			}
+
+			builder.Append(" From Tail : ");
+
+			traveler = Tail;
+			while (traveler != null)
+			{
+				builder.Append(" <- ");
+				builder.Append(traveler.Value);
+				traveler = traveler.Previous;
+			}
+
+			return builder.ToString();
+		}
+
+		public class Node
+		{
+
+			public Node Next;
+
+			public int Value;
+
+			public Node Previous;
+
+		}
+	}
+}
--- Ethan/Program.cs
+++ Ethan/Program.cs
@@ -10,6 +10,108 @@ namespace Ethan
         public static void Main(string[] args)
         {
 
+            DblLinkedListTests();
+
+        }
+
+        static void DblLinkedListTests() {
+
+            DoubleLinkedList list = new DoubleLinkedList();
+            
+            list.AddToTail(9);
+            list.AddToTail(18);
+            list.AddToTail(27);
+			list.AddToTail(36);
+
+			Console.WriteLine(list);
+
+            list.Remove(36);
+
+            Console.WriteLine(list);
+
+        }
+
+        static void LinkedListTests()
+        {
+            
+            SingleLinkedList list = new SingleLinkedList();
+
+            Console.WriteLine("Empty list length is..." + list.Count);
+
+            list.AddToBeginning(555);
+            list.AddToBeginning(5555);
+			list.AddToEnd(5);
+            list.AddToEnd(55);
+            list.AddToBeginning(55555);
+            list.AddToEnd(2);
+            Console.WriteLine("Searching for: 5" + list.Contains(5));
+            Console.WriteLine("Searching for: 7" + list.Contains(7));
+            Console.WriteLine("Searching for: 2" + list.Contains(2));
+            Console.WriteLine("List length is..." + list.Count);
+            Console.WriteLine(list);
+
+        }
+
+
+
+        static void BinarySearchTests()
+        {
+
+            List<int> test = new List<int>();
+
+            test.Add(10);
+            test.Add(18);
+            test.Add(32);
+            test.Add(64);
+            test.Add(66);
+            test.Add(151);
+            test.Add(250);
+            test.Add(1001);
+
+            Console.WriteLine("Found 10000000 at " + ListTools.BinarySearch(test, 60));
+
+        }
+
+        static void CooldownTests()
+        {
+
+            Cooldown c = new Cooldown(5);
+
+            Console.WriteLine("Interval is " + c.GetInterval() + ", should be 5");
+
+            Console.WriteLine("Trying to run at 1, success: " + c.Attempt(1));
+
+            Console.WriteLine("Resetting!");
+            c.Reset();
+
+            Console.WriteLine("Trying to run at 2, success: " + c.Attempt(2));
+
+            Console.WriteLine("At 3, time until next action is " + c.GetTimeUntilNextAction(3));
+
+            Console.WriteLine("Trying to run at 3, success: " + c.Attempt(3));
+
+            Console.WriteLine("Trying to run at 5.99, success: " + c.Attempt(5.99f));
+
+            Console.WriteLine("Trying to run at 15, success: " + c.Attempt(15));
+
+        }
+
+        static void StackTests() {
+
+            SimpleStack s = new SimpleStack(5);
+
+            for (int i = 0; i < 20; i++)
+            {
+                s.Push(2);
+            }
+
+            s.PrintUnderlying();
+
+
+        }
+
+        static void QueueTests() {
+
             // Simple text case for SimpleQueue
 
             SimpleQueue s = new SimpleQueue();
@@ -35,7 +137,6 @@ namespace Ethan
             Console.WriteLine("Dequeue:" + s.Dequeue() + ", should be 30");
 
             Console.WriteLine("Count:" + s.Count() + ", should be 0");
-
         }
     }
 }
